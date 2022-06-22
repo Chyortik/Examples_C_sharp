02995567bfc6dfb894e2d13104b624479c29c9ad
Dmitriy <dimon_smirnov81@mail.ru>
2022-06-22 23:03:40 +0300

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dimon")
+{
+    Console.WriteLine("Салют, браток!!!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
